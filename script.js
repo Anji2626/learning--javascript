e69b7d3736ac2111483bbe5a0bb0373af9a57e16
anji <anjijonnada2626@gmail.com>
2026-04-16 09:48:20 +0530

    added new

--- script.js
+++ script.js
@@ -262,14 +262,62 @@
 // }
 
 // const myFun = () => {
-//     let display = document.getElementById("buchi"); 
+//     let display = document.getElementById("img"); 
 //    if(display.src === "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrfkCEjWkINAy1xTHx2kOQqJeParscyDTydA&s"){
 //         display.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjVw4_OCGfr2dcKp4sJiFjg2VFNNRGY_Gxjg&s"
 //    }
 //    else
 //     display.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrfkCEjWkINAy1xTHx2kOQqJeParscyDTydA&s"
-
 // }
+
+// const add = () =>{
+//     event.preventDefault()
+//     let val1 = document.getElementById("num1").value
+//     let val2 = document.getElementById("num2").value
+//     let display = document.getElementById("result");
+
+//     let num1 = parseInt(val1)
+//     let num2 = parseInt(val2)
+
+//     display.innerHTML = `The sum of ${num1} and ${num2} is ${num1 + num2}`
+// } 
+
+// const sub = () =>{
+//     event.preventDefault()
+//     let val1 = document.getElementById("num1").value
+//     let val2 = document.getElementById("num2").value
+//     let display = document.getElementById("result");
+
+//     let num1 = parseFloat(val1)
+//     let num2 = parseFloat(val2)
+
+//     display.innerHTML = `The sub of ${num1} and ${num2} is ${num1 - num2}`
+// } 
+
+// const mul = () =>{
+//     event.preventDefault()
+//     let val1 = document.getElementById("num1").value
+//     let val2 = document.getElementById("num2").value
+//     let display = document.getElementById("result");
+
+//     let num1 = parseInt(val1)
+//     let num2 = parseInt(val2)
+
+//     display.innerHTML = `The mul of ${num1} and ${num2} is ${num1 * num2}`
+// } 
+
+// const div = () =>{
+//     event.preventDefault()
+//     let val1 = document.getElementById("num1").value
+//     let val2 = document.getElementById("num2").value
+//     let display = document.getElementById("result");
+
+//     let num1 = parseFloat(val1)
+//     let num2 = parseFloat(val2)
+
+//     display.innerHTML = `The div of ${num1} and ${num2} is ${num1 / num2}`
+// } 
+
 
 
 
